1aaf53babcb072b713134145cf379ce8f056a60d
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-16 07:57:26 -0300

    feat: Add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./i18n/i18n"
+import "./i18n/i18n";
 import GlobalStyle from "./Styles/GlobalStyles";
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./Styles/Themes/darkTheme";
+import { lightTheme } from "./Styles/Themes/lightTheme";
+import ChangeThemeToggler from "./Components/ChangeThemeToggler/ChangeThemeToggler";
 
 const App: React.FC = () => {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
     <div className="App">
-      <GlobalStyle />
-      <Outlet />
+      <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <Outlet />
+        <ChangeThemeToggler themeToggler={themeToggler} />
+      </ThemeProvider>
     </div>
   );
 };
