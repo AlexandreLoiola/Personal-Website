8049c1736989eb2eb40c60cc5117c7003536bbfe
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-16 08:33:53 -0300

    feat: Create Home Page

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Container,
+  StyledContainerButton,
+  StyledOverLine,
+  StyledPrimaryButton,
+  StyledSecundaryButton,
+  StyledSpan,
+  StyledSubTitleName,
+  StyledTitleName,
+} from "./styles";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <StyledOverLine>👋{t("Hello i am")}</StyledOverLine>
+      <StyledTitleName>{t("Alexandre Loiola")}</StyledTitleName>
+      <StyledSubTitleName>
+        {t("Systems Analyst - Fullstack Developer")}
+      </StyledSubTitleName>
+      <StyledSpan>
+        {"\u00A0\u00A0\u00A0"}
+        {t(
+          "I work as a Fullstack Developer (Backend-Heavy), with a degree in Systems Analysis and Development from the Federal Institute of Sergipe - IFS. I am a dedicated professional and passionate about technology, always in search of new challenges."
+        )}
+      </StyledSpan>
+      <StyledContainerButton>
+        <StyledPrimaryButton onClick={() => navigate("/portfolio")}>
+          {t("My Projects")}
+        </StyledPrimaryButton>
+        <StyledSecundaryButton onClick={() => navigate("/contact")}>
+          {t("Get in Touch")}
+        </StyledSecundaryButton>
+      </StyledContainerButton>
+    </Container>
+  );
+};
+export default Home;
---
+++ src/Pages/Home/styles.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { PrimaryButton, secundaryButton } from "../../Styles/GlobalStyles";
+import { ButtonProps } from "react-bootstrap";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 50vw;
+  margin-top: 10vh;
+`;
+
+export const StyledOverLine = styled.span`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin-left: 1vw;
+  color: ${(props) => props.theme.tertiary};
+  font-size: 22px;
+  text-align: left;
+`;
+
+export const StyledTitleName = styled.h1`
+  font-family: Montserrat;
+  font-size: 78px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin: 0;
+  color: ${(props) => props.theme.tertiary};
+  font-family: Montserrat;
+  text-align: left;
+`;
+
+export const StyledSubTitleName = styled.span`
+  font-family: Mada;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  color: ${(props) => props.theme.secundary};
+`;
+
+export const StyledSpan = styled.span`
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin: 20px 0;
+  max-width: 44vw;
+  text-align: justify;
+  color: ${(props) => props.theme.tertiary};
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+export const StyledPrimaryButton = styled(PrimaryButton as React.FC<ButtonProps>)`
+  width: 150px;
+`;
+
+export const StyledSecundaryButton = styled(secundaryButton as React.FC<ButtonProps>)`
+  width: 150px;
+`;
+
+export const StyledContainerButton = styled.div`
+  display: flex;
+  columns: row;
+  gap: 25px;
+  margin: 25px 0;
+`;
